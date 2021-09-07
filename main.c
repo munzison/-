56f4fe325ac43deba89a97de55ae965011019c30
ムンジソン イソジン <munziso1024@gmail.com>
2021-09-07 09:43:23 +0900

    プロジェクト ファイルを追加します。

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+printf_(int num){
+	//for (int i = 0;);
+	printf("הKדכה`\n");
+}
+main() {
+	struct blackjack {
+		int AK;
+		int KK;
+		int KK2;
+	};
+	struct blackjack card;
+	card.AK = 21;
+	card.KK2 = 22;
+	printf("%d\n", card.AK);
+	printf("%d\n", card.KK2);
+	printf_(3);
+	_getch();
+}
